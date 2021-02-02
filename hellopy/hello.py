779a3d1cf1bb8c1ee08aad1139031160b1da78e8
zuhthisahan <zuhthisahan7@gmail.com>
2021-02-02 16:08:44 +0530

    Grab parcel_number from cmd args

--- hellopy/hello.py
+++ hellopy/hello.py
@@ -1,3 +1,9 @@
-print("### Building permit info for placer country ### \n")
-print("Please Enter a parcel number to search for ")
-print("For Example: 042-032-015-000")
+import sys
+
+print("### Building permit info for placer country ###")
+if len(sys.argv) == 1:
+    print("Please Enter a parcel number to search for ")
+    print("For Example: 042-032-015-000")
+else:
+    print("Parcel number: " + str(sys.argv[1]))
+    print("Looking up, building permits...")
