33916e11257d15679ae1f96b8515075f537b421d
zuhthisahan <zuhthisahan7@gmail.com>
2021-02-03 13:49:29 +0530

    Create the start_request function to fetch the data

--- webscrapy/webscrapy/spiders/placerspider.py
+++ webscrapy/webscrapy/spiders/placerspider.py
@@ -2,3 +2,11 @@ import scrapy
 
 class PlacerSpider(scrapy.Spider):
     name = 'placer'
+
+    # create the function to get the information
+    def start_requests(self):
+        url = 'https://permits.placer.ca.gov/CitizenAccess/Default.aspx'
+
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    
